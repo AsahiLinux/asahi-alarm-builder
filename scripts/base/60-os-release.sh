980ca1b9fbf9f301296cc286215ec60a5a6dc056
Adryzz <adryzz@vignetteapp.org>
2022-03-23 13:28:30 +0100

    Remove slash for consistency
    Signed-off-by: Adryzz <adryzz@vignetteapp.org>

--- scripts/base/60-os-release.sh
+++ scripts/base/60-os-release.sh
@@ -6,7 +6,7 @@ NAME="Asahi Linux"
 ID=asahi
 ID_LIKE=arch
 BUILD_ID=rolling
-HOME_URL="https://asahilinux.org/"
+HOME_URL="https://asahilinux.org"
 DOCUMENTATION_URL="https://github.com/asahilinux/docs/wiki"
 BUG_REPORT_URL="https://github.com/AsahiLinux"
 EOF
