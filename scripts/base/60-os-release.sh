d15eb8dd78598a724a8111fd186734eb73637e0b
Adryzz <adryzz@vignetteapp.org>
2022-03-23 10:25:06 +0100

    Add /etc/os-release
    Signed-off-by: Adryzz <adryzz@vignetteapp.org>

---
+++ scripts/base/60-os-release.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+cat >/etc/os-release <<EOF
+NAME="Asahi Linux"
+ID=asahi
+ID_LIKE=arch
+BUILD_ID=rolling
+HOME_URL="https://asahilinux.org/"
+DOCUMENTATION_URL="https://github.com/asahilinux/docs/wiki"
+BUG_REPORT_URL="https://github.com/AsahiLinux"
+EOF
