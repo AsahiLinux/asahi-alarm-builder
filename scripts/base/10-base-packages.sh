241287e570e243b473afd3352281b2d05013db4b
Hector Martin <marcan@marcan.st>
2022-11-22 04:02:03 +0900

    base/10-base-packages.sh: Set up /boot for fake-ESP m1n1 install
    Signed-off-by: Hector Martin <marcan@marcan.st>

--- scripts/base/10-base-packages.sh
+++ scripts/base/10-base-packages.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -e
 
-mkdir -p "/boot/efi"
+mkdir -p "/boot/efi/EFI/BOOT"
+mkdir -p "/boot/efi/m1n1"
+touch "/boot/efi/.builder"
 
 pacman --noconfirm -R linux-aarch64
 pacman --noconfirm -Syu
